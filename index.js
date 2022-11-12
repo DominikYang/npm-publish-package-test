5712f18feeb3d0d1d7dfba471d5a759f5bc7e21c
chuck_wei <wyy453822487@qq.com>
2022-11-12 22:48:59 +0800

    feat: init project

---
+++ index.js
@@ -0,0 +1,6 @@
+function add(num1, num2) {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') throw new Error('error type of value input')
+    return num1 + num2
+}
+
+module.export.add = add
