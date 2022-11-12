53099b8d42210a83754e86b7638094582df233bb
chuck_wei <wyy453822487@qq.com>
2022-11-12 23:12:31 +0800

    fix: switch to es module

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function add(num1, num2) {
+export function add(num1, num2) {
     if(typeof num1 !== 'number' || typeof num2 !== 'number') throw new Error('error type of value input')
     return num1 + num2
 }
-
-module.export.add = add
